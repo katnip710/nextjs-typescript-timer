29f69b2c28e36cab4df4c827b937abff9c53dd43
Kat Lea <katarina.lea@globacore.com>
2022-08-03 17:15:45 -0400

    spooky updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
   const [seconds, setSeconds] = useState(0)
 
   useEffect(() =>{
-    //const target = new Date('10/30/2022 23:59:59')
-    const target = new Date ('08/03/2022 12:56:00')
+    const target = new Date('10/30/2022 23:59:59')
+    //const target = new Date ('08/03/2022 4:53:00')
 
     const interval = setInterval(() => {
       const now = new Date()
@@ -54,31 +54,48 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {spookyTime ? (
-        <h1>Happy Halloween!</h1>
-      ) : (
+        {/* Ani Moon Background */}
+        <div className="background-container">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="moon" className="moon"></img>
+          <div className="stars"></div>
+          <div className="twinkling"></div>
+          <div className="clouds"></div>
+        </div>
+        {/* Ani Moon Background */}
 
-      <div className="timer-wrapper">
-        <div className="timer-inner">
-          <div className="timer-segment">
-            <span className="time">{days}</span>
-            <span className="label">Days</span>
+      <main>
+        {spookyTime ? (
+          <div className="halloweenTime">
+            <iframe width="560" height="349" src="https://www.youtube.com/embed/8wUixhV13gI?autoplay=1&mute=1&showinfo=0&controls=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+            <h1>Happy Halloween!</h1>
           </div>
-          <div className="timer-segment">
-            <span className="time">{hours}</span>
-            <span className="label">Hours</span>
-          </div>
-          <div className="timer-segment">
-            <span className="time">{minutes}</span>
-            <span className="label">Minutes</span>
-          </div>
-          <div className="timer-segment">
-            <span className="time">{seconds}</span>
-            <span className="label">Seconds</span>
+        ) : (
+
+        <div className="timer-wrapper">
+          <h2>Countdown to Halloween</h2>
+
+          <div className="timer-inner">
+            <div className="timer-segment">
+              <span className="time">{days}</span>
+              <span className="label">Days</span>
+            </div>
+            <div className="timer-segment">
+              <span className="time">{hours}</span>
+              <span className="label">Hours</span>
+            </div>
+            <div className="timer-segment">
+              <span className="time">{minutes}</span>
+              <span className="label">Minutes</span>
+            </div>
+            <div className="timer-segment">
+              <span className="time">{seconds}</span>
+              <span className="label">Seconds</span>
+            </div>
           </div>
         </div>
-      </div>
-      )}
+        )}
+      </main>
     </>
   );
 };

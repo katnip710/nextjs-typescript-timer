5f1caa58aa4970d92dba79b32bddaf71a7d2f8c9
Kat Lea <katarina.lea@globacore.com>
2022-08-08 16:16:45 -0400

    cleaned up code

---
+++ src/components/MoonBackground.tsx
@@ -0,0 +1,18 @@
+function Moon() {
+
+    return (
+        <>
+            {/* Ani Moon Background */}
+            <div className="background-container">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="moon" className="moon" layout='fill'></img>
+                <div className="stars"></div>
+                <div className="twinkling"></div>
+                <div className="clouds"></div>
+            </div>
+            {/* Ani Moon Background */}
+        </>
+    );
+};
+
+export default Moon;
+

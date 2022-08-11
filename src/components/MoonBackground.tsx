736e51ddab4d597902f378767b3cda8f8db9b167
Kat Lea <katarina.lea@globacore.com>
2022-08-11 15:22:06 -0400

    fix

--- src/components/MoonBackground.tsx
+++ src/components/MoonBackground.tsx
@@ -4,7 +4,7 @@ function Moon() {
         <>
             {/* Ani Moon Background */}
             <div className="background-container">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="moon" className="moon" layout='fill'></img>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="moon" className="moon" />
                 <div className="stars"></div>
                 <div className="twinkling"></div>
                 <div className="clouds"></div>

42f7cb77065fe1cbf9a5be9b3a0a01f613feb18b
Kat Lea <katarina.lea@globacore.com>
2022-08-12 14:00:28 -0400

    working on audio element

--- src/components/SpookyMusic.tsx
+++ src/components/SpookyMusic.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 function SpookyMusic() {
+
+    const [playMusic, setplayMusic] = useState(false)
+    const stopMusic = () => setplayMusic(true);
+    const triggerMusic = () => {
+        setplayMusic(false)
+
+    }
 
     return (
         <>
+            <audio src="/audio/SpookyMusic.mp3"></audio>
+
+            <div>
+                <button onClick={stopMusic} style={{
+                    display: playMusic ? 'none' : 'block'
+                }}>Stop</button>
+                <button onClick={triggerMusic} style={{
+                    display: playMusic ? 'block' : 'none'
+                }}>Play</button>
+            </div>
 
         </>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,55 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Moon from "../components/MoonBackground";
-import Timer from "../components/Timer";
+import SpookyMusic from "../components/SpookyMusic";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+
+  const [start, setStart] = useState(false)
+  const openTimer = () => setStart(true);
+
+  const [spookyTime, setSpookyTime] = useState(false)
+
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect(() =>{
+      const target = new Date('10/30/2022 23:59:59')
+      //const target = new Date ('08/12/2022 1:51:00')
+
+      const interval = setInterval(() => {
+      const now = new Date()
+      const difference = target.getTime() - now.getTime()
+
+      const d = Math.floor(difference / (1000 * 60 * 60 *24))
+      setDays(d)
+
+      const h = Math.floor(
+          (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
+      setHours(h)
+
+      const m = Math.floor(
+          (difference % (1000 * 60 * 60)) / (1000 * 60)
+      )
+      setMinutes(m)
+
+      const s = Math.floor(
+          (difference % (1000 * 60)) / 1000
+      )
+      setSeconds(s)
+
+      if(d <= 0 && h <= 0 && m <= 0 && s <= m){
+          setSpookyTime(true)
+      }
+
+      }, 1000)
+
+      return () => clearInterval(interval)
+  }, [])
 
   return (
     <>
@@ -13,9 +59,51 @@ const Home: NextPage = () => {
         <link rel="icon" href="img/FullMoon.png" />
       </Head>
 
+      <div className="start-timer-button" style={{
+        display: start ? 'none' : 'flex',
+      }}>
+        <h1 onClick={openTimer}>Start Countdown to Halloween</h1>
+      </div>
+
       <Moon />
 
-      <Timer />
+      <main style={{
+        display: start ? 'flex' : 'none',
+      }}>
+        {spookyTime ? (
+            <div className="halloweenTime">
+                <iframe width="560" height="349" src="https://www.youtube.com/embed/8wUixhV13gI?autoplay=1&mute=1&showinfo=0&controls=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+                <h1>Happy Halloween!</h1>
+            </div>
+            ) : (
+
+            <div className="timer-wrapper">
+                <h2>Countdown to Halloween</h2>
+
+                <div className="timer-inner">
+                    <div className="timer-segment">
+                        <span className="time">{days}</span>
+                        <span className="label">Days</span>
+                    </div>
+                    <div className="timer-segment">
+                        <span className="time">{hours}</span>
+                        <span className="label">Hours</span>
+                    </div>
+                    <div className="timer-segment">
+                        <span className="time">{minutes}</span>
+                        <span className="label">Minutes</span>
+                    </div>
+                    <div className="timer-segment">
+                        <span className="time">{seconds}</span>
+                        <span className="label">Seconds</span>
+                    </div>
+                </div>
+
+                <SpookyMusic />
+            </div>
+        )}
+        </main>
     </>
   );
 };
